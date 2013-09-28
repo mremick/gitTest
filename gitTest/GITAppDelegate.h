496865c500a00e4ff6569cc2b21a1c9023ebb344
Remm1 <mremick@gmail.com>
2013-09-27 19:22:15 -0700

    Initial Commit

---
+++ gitTest/GITAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GITAppDelegate.h
+//  gitTest
+//
+//  Created by Matt Remick on 9/27/13.
+//  Copyright (c) 2013 Matt Remick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GITAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ gitTest/GITViewController.h
@@ -0,0 +1,13 @@
+//
+//  GITViewController.h
+//  gitTest
+//
+//  Created by Matt Remick on 9/27/13.
+//  Copyright (c) 2013 Matt Remick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GITViewController : UIViewController
+
+@end
